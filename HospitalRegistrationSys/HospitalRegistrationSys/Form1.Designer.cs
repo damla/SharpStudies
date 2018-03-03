7319c35d2b20455a7af5a7d3344c12abdf91cbd5
Damla Koksal <damlakoksal@outlook.com>
2018-03-04 00:27:55 +0300

    Hospital Registration System example added. Add button setted.

---
+++ HospitalRegistrationSys/HospitalRegistrationSys/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace HospitalRegistrationSys
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtClinic = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblClinic = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lbPatients = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(51, 102);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(302, 31);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(51, 260);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(302, 31);
+            this.txtAge.TabIndex = 1;
+            // 
+            // txtClinic
+            // 
+            this.txtClinic.Location = new System.Drawing.Point(51, 432);
+            this.txtClinic.Name = "txtClinic";
+            this.txtClinic.Size = new System.Drawing.Size(302, 31);
+            this.txtClinic.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblName.Location = new System.Drawing.Point(44, 50);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(270, 31);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name and Surname";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAge.Location = new System.Drawing.Point(44, 211);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(65, 31);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "Age";
+            // 
+            // lblClinic
+            // 
+            this.lblClinic.AutoSize = true;
+            this.lblClinic.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblClinic.Location = new System.Drawing.Point(44, 383);
+            this.lblClinic.Name = "lblClinic";
+            this.lblClinic.Size = new System.Drawing.Size(87, 31);
+            this.lblClinic.TabIndex = 5;
+            this.lblClinic.Text = "Clinic";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(401, 143);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(264, 66);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "ADD PATIENT TO LIST";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(401, 335);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(264, 66);
+            this.btnRemove.TabIndex = 7;
+            this.btnRemove.Text = "REMOVE PATIENT FROM LIST";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // lbPatients
+            // 
+            this.lbPatients.FormattingEnabled = true;
+            this.lbPatients.ItemHeight = 25;
+            this.lbPatients.Location = new System.Drawing.Point(690, 28);
+            this.lbPatients.Name = "lbPatients";
+            this.lbPatients.Size = new System.Drawing.Size(326, 554);
+            this.lbPatients.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1054, 627);
+            this.Controls.Add(this.lbPatients);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblClinic);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtClinic);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Hospital Registration System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtClinic;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblClinic;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ListBox lbPatients;
+    }
+}
+
---
+++ HospitalRegistrationSys/HospitalRegistrationSys/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HospitalRegistrationSys
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool validName = txtName.Text.All(Char.IsLetter); //Checking if user enters number in name.
+            bool validClinic = txtClinic.Text.All(Char.IsLetter);
+
+            if (txtName.Text.Equals("") && txtAge.Text.Equals("") && txtClinic.Text.Equals(""))
+                MessageBox.Show("Make sure that you fill given fields.");
+            else if (!validName)
+                MessageBox.Show("Enter the Name of patient properly.");
+            else if (!validClinic)
+                MessageBox.Show("Enter the clinic information properly.");
+            //Wrote validateAge() method for age control.
+            else if (!validateAge())
+                MessageBox.Show("Age should be a number.");
+            else if (lbPatients.Items.Contains(txtName.Text + " " + txtAge.Text + " " + txtClinic.Text))
+            {
+                MessageBox.Show("This patient is already added to the list.");
+                //remove method wrote for clearing the fields.
+                removeTextBoxes();
+            }
+            else //everything is okay, add it to the list
+            {
+                lbPatients.Items.Add(txtName.Text + " " + txtAge.Text + " " + txtClinic.Text);
+                removeTextBoxes(); //Clear the fields.
+            }
+        }
+        private bool validateAge()
+        {
+            int parsed;
+            if (!int.TryParse(txtAge.Text, out parsed))
+                return false;
+            else
+                return true;
+        }
+
+        private void removeTextBoxes()
+        {
+            txtName.Text = "";
+            txtAge.Text = "";
+            txtClinic.Text = "";
+        }
+    }
+}
