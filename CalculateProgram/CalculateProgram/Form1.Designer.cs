b368539c9920b2e4853333d9531cd3249cf9318d
Damla Koksal <damlakoksal@outlook.com>
2018-04-22 15:05:11 +0300

    Added a button to make dynamic buttons and textBoxes. After you click that button, it randomize number in textboxes and if you click calculate linklabel, it will sum all of the numbers.

---
+++ CalculateProgram/CalculateProgram/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CalculateProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ADD A NEW CONTROL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 103);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(548, 394);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(615, 103);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(534, 394);
+            this.panel2.TabIndex = 2;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(610, 51);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(136, 25);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "CALCULATE";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(529, 509);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(82, 25);
+            this.linkLabel2.TabIndex = 4;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "CLEAR";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 543);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ CalculateProgram/CalculateProgram/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dynamicButtonClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender; //buttona tiklandiginda senderdan info alinmis oluyor.
+
+            Control[] txts = panel2.Controls.Find(btn.Name.Replace("btn", "txt"), true); //obje olarak ismi btn olani txt olarak degistir.
+            // MessageBox.Show(((TextBox)txts[0]).Text);
+            for (int i = 0; i < panel2.Controls.Count; i++)
+            {
+                TextBox txt = (TextBox)panel2.Controls[i];
+                if (txt.Name == btn.Name.Replace("btn", "txt"))
+                {
+                    MessageBox.Show(txt.Text);
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            int sum = 0;
+
+            for(int i=0; i< panel2.Controls.Count; i++)
+            {
+                TextBox txt = (TextBox)panel2.Controls[i];
+                sum += Convert.ToInt32(txt.Text);
+            }
+            MessageBox.Show("the sum is: " + sum); //controllerin icindeki textleri cekip integera pars edip topladi.
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            panel1.Controls.Clear();
+            panel2.Controls.Clear();
+            x = y = 0; //konumlari da sifirlams olduk.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int x = 0, y = 0;
+        private void button1_Click(object sender, EventArgs e) //button1'e tiklanildiginda bu ozelliklere sahip button ve textBox olusturuyor.
+        {
+            Button btn = new Button(); //btn adli button objesii
+            btn.Text = "click!"; //Ustunde yazan click!
+            btn.Name = "btn_" + x + "_" + y; //ismi konumuna gore degisiyor
+            btn.Size = new Size(40, 25); //boyutu sabit 
+            btn.Location = new Point(x * 50, y * 30); // konumu x ve y degerlerinin katlari seklinde belirlendiki surekli degisim saglasin.
+            btn.Click += new System.EventHandler(dynamicButtonClick); //yaratilan buttona tiklandgnda ilk method olan dynamicbutton'a yolluyoruz.
+                                                                      //bu buttona tiklandiginda hedef buttonun baglantili oldugu sayiyi gostermesi.
+
+            TextBox txt = new TextBox();
+            txt.Text = new Random().Next(100).ToString(); //100'e kadar sayi secildi. Tusa her tiklanildiginda bu sayede icine random sayilar konulacak. (String olduklarini unutma!)
+            txt.Name = "txt_" + x + "_" + y; //isim kordinata gore
+            txt.Size = new System.Drawing.Size (40, 25); //textbox olmasindan dolayi drawing.size kutuphanesi kullandildi.
+            txt.Location = new Point(x * 50, y * 30); //yine location x ve y degerlerine gore ayarlanacak.
+
+            panel1.Controls.Add(btn); //button brinci panele eklendi.
+            panel2.Controls.Add(txt); //txt ikinci panele eklendi.
+            x++; //x her button1 e tiklanildiginda artiyor.
+            if (x == 5) //5'e esitlendiginde alt satira gecmesi icin y artiyor ve x sifirlaniyorki satir basina gelsin.
+            {
+                y++;
+                x = 0;
+            }
+        }    
+    }
+}
+/*
+ * DynamicButton methodu ile sonradan olusan buton methodu olusturduk.
+ * Button1'e tiklandiginda bu methodumuz calisacak.
+ */
