e858d37f0a7ee29c503f72a1e26e8e1956425c34
Damla Koksal <damlakoksal@outlook.com>
2019-05-30 18:28:01 +0300

    New demo added as an interface example.

---
+++ Inheritence_or_Interface/InterfacesDemo/IWorker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    interface IWorker
+    {
+        void Work();
+    }
+    interface IEat
+    {
+        void Eat();
+    }
+    interface ISalary
+    {
+        void GetSalary();
+    }
+    
+    // We may implement interfaces more than 1.
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Manager ate.");
+        }
+
+        public void GetSalary()
+        {
+      
+        }
+
+        public void Work()
+        {
+            
+        }
+    }
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Worker ate.");
+        }
+
+        public void GetSalary()
+        {
+            
+        }
+
+        public void Work()
+        {
+            
+        }
+    }
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            
+        }
+    }
+}
---
+++ Inheritence_or_Interface/InterfacesDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[] { new Worker(), new Manager(), new Robot() };
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[] { new Worker(), new Manager() };
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+        }
+    }
+}
+
+/*
+ * Demo information:
+ * We have a company made of workers, robots and managers.
+ * Create interface structure for this company.
+ */
