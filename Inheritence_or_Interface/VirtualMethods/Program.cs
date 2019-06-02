1eb527eec5220198198e048b87fe37522a404205
Damla Koksal <damlakoksal@outlook.com>
2019-06-02 14:56:17 +0300

    Virtual Methods added.

--- Inheritence_or_Interface/VirtualMethods/Program.cs
+++ Inheritence_or_Interface/VirtualMethods/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace VirtualMethods
 {
+    // Important subject to understand inheritence.
     class Program
     {
         static void Main(string[] args)
         {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+            MySql mySql = new MySql();
+            mySql.Add();
+
+            Console.ReadLine();
         }
+    }
+
+    class Database
+    {
+        // by the virtual methods, we have ability to override.
+        public virtual void Add() 
+        {
+            Console.WriteLine("Added by default.");
+        }
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by default.");
+        }
+    }
+    class SqlServer : Database
+    {
+        public override void Add()
+        {
+            base.Add(); // base means Database(parent)
+        }
+    }
+    class MySql : Database
+    {
+
     }
 }
